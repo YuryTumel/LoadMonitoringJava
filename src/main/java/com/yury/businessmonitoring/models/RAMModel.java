9790a33d6c4ffe5f9f23d7c719215507c07a788e
YuryTumel <acckiymedwed@gmail.com>
2015-10-04 01:21:44 +0300

    annotations added

--- src/main/java/com/yury/businessmonitoring/models/RAMModel.java
+++ src/main/java/com/yury/businessmonitoring/models/RAMModel.java
@@ -7,18 +7,31 @@ package com.yury.businessmonitoring.models;
 
 import com.yury.businessmonitoring.interfaces.IModel;
 import java.time.LocalDateTime;
-import java.util.Date;
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  *
  * @author Юрий
  */
+@Entity
+@Table(name = "ram")
 public class RAMModel extends IModel{
+    @Id
+    @Column(name = "id")
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private int ID;
+    
+    @Column(name = "free_memory", nullable = false)
     private int freeMemory;
+    
+    @Column(name = "date", nullable = false)
     private LocalDateTime date;
     
-    public RAMModel (int ID, int freeMemory, LocalDateTime date) {
+    public RAMModel() {}
+    
+    public RAMModel(int ID, int freeMemory, LocalDateTime date) {
         this.ID = ID;
         this.freeMemory = freeMemory;
         this.date = date;
@@ -28,45 +41,27 @@ public class RAMModel extends IModel{
         this.freeMemory = freeMemory;
         this.date = LocalDateTime.now();
     }
-
-    /**
-     * @return the ID
-     */
+    
     public int getID() {
         return ID;
     }
 
-    /**
-     * @param ID the ID to set
-     */
     public void setID(int ID) {
         this.ID = ID;
     }
 
-    /**
-     * @return the date
-     */
     public LocalDateTime getDate() {
         return date;
     }
 
-    /**
-     * @param date the date to set
-     */
     public void setDate(LocalDateTime date) {
         this.date = date;
     }
 
-    /**
-     * @return the freeMemory
-     */
     public int getFreeMemory() {
         return freeMemory;
     }
 
-    /**
-     * @param freeMemory the freeMemory to set
-     */
     public void setFreeMemory(int freeMemory) {
         this.freeMemory = freeMemory;
     }
